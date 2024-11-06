69e3dea687f950b9eaf4eb7e18aea8f891f7b0f1
ICR07 <112337111+ICR07@users.noreply.github.com>
2024-11-06 11:11:58 -0500

    Tarea lista

--- homework/word_count.py
+++ homework/word_count.py
@@ -4,8 +4,10 @@
 
 import fileinput
 import glob
+import string
 import os.path
 from itertools import groupby
+
 
 
 #
@@ -23,8 +25,16 @@ from itertools import groupby
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
+
 def load_input(input_directory):
     """Funcion load_input"""
+    sequence = []
+    files = glob.glob(f"{input_directory}/*")
+    with fileinput.input(files=files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
+
 
 
 #
@@ -34,6 +44,12 @@ def load_input(input_directory):
 #
 def line_preprocessing(sequence):
     """Line Preprocessing"""
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation)).lower().strip())
+        for key, value in sequence
+    ]
+    return sequence
+  
 
 
 #
@@ -48,9 +64,15 @@ def line_preprocessing(sequence):
 #     ...
 #   ]
 #
+   
 def mapper(sequence):
     """Mapper"""
-
+    # result = []
+    # for _, value in sequence:
+    #     for word in value.split():
+    #         result.append( (word, 1) )  
+    # return result
+    return [(word, 1) for _, value in sequence for word in value.split()]
 
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
@@ -65,6 +87,7 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -75,6 +98,12 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+    result = {}
+    for key, value in sequence:
+        if key not in result.keys():
+            result[key] = 0
+        result[key] += value
+    return list(result.items())
 
 
 #
@@ -83,6 +112,11 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 
 #
@@ -95,7 +129,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
-
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 #
 # La siguiente función crea un archivo llamado _SUCCESS en el directorio
@@ -103,6 +139,8 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
@@ -110,10 +148,20 @@ def create_marker(output_directory):
 #
 def run_job(input_directory, output_directory):
     """Job"""
-
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+    
 
 if __name__ == "__main__":
     run_job(
-        "input",
-        "output",
+        "files/input",
+        "files/output",
     )
+  
+
